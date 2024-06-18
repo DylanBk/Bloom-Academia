91811520d6da4f894b66971c773a0255a4529d02
Dylan <dylan.bullock.965@accesscreative.ac.uk>
2024-06-18 20:15:05 +0100

    added route for index.html

--- app.py
+++ app.py
@@ -2,5 +2,10 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
 if __name__ == "__main__":
     app.run()
